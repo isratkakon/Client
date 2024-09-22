0fd10272b1f1bccd41f6298fcd131214532b64a7
isratkakon <isratkakon0@gmail.com>
2024-09-22 10:18:14 +0600

    frontend_design

---
+++ components/AppLogo.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <NuxtLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LaraNuxtAuth</span>
+        </NuxtLink>
+    </div>
+</template>
---
+++ pages/auth/dashboard.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8">
+        <h5 class="mb-2 text-3xl font-bold text-gray-900">Dashboard</h5>  
+        <p class="mb-5 text-base text-gray-500 sm:text-lg ">This is my dashboard</p>
+    </div>
+</template>
